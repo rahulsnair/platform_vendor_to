53d501a82f89f2ec921ae580711e35e726a5152a
RAHUL S NAIR <rahulsnair30@gmail.com>
2016-10-08 08:31:01 +0530

    Add athene support

---
+++ product/moto/athene/to.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from meliusltexx device
+$(call inherit-product, device/motorola/athene/device.mk)
+
+# Inherit some common teamoctos stuff.
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+# Also get non-open-source specific aspects
+$(call inherit-product, vendor/motorola/msm8952/athene/athene-vendor.mk)
+
+# Boot animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 1920
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := athene
+PRODUCT_NAME := to_athene
+PRODUCT_BRAND := Motorola
+PRODUCT_MANUFACTURER := Motorola
+PRODUCT_RELEASE_NAME := athene
